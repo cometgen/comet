02b33c146bc757dab6901088ae705ad198664596
cometgen <117958288+cometgen@users.noreply.github.com>
2022-11-12 21:39:29 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,23 @@
 import time
 
-cum = "Are"
-In = "you"
-me = "gay"
+cum = "Kill All Jews."
+In = "Kill All Faggots."
+me = "Kill All Niggers."
+ih8nig = time
+blackmen = ih8nig
+faggots = blackmen
+jews = faggots
+hitler = jews
+hail = print
+fag = hail
+killniggers = fag
+youaregay = killniggers
+ihateniggersandfaggots = youaregay
 
-print(f"{In}")
-time.sleep(1)
-print(f"{cum}")
-time.sleep(1)
-print(f"{me}")
+
+
+ihateniggersandfaggots(f"{In}")
+hitler.sleep(1)
+ihateniggersandfaggots(f"{cum}")
+hitler.sleep(1)
+ihateniggersandfaggots(f"{me}")
